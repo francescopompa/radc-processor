fa62166e838ade92dccd0d6d243a09fcc4829c75
Ulrich Pirling <ulrich.pirling@student.kit.edu>
2023-03-16 13:20:55 +0100

    switched receiver to threading instead of mp

--- udp_receiver/receiver_class.py
+++ udp_receiver/receiver_class.py
@@ -2,23 +2,21 @@ import socket
 import sys
 import os
 import time
-import multiprocessing as mp
+import threading as thr
+import queue
 import signal
 
-#
-# Todo:
-# - Migrate to threading and queue modules? (more lightweight and IO focused)
-#
 
 class Receiver():
 
     def __init__(self,
         target_file="testreadoutfile.bin",
-        host="", port=50007,
+        host="", port=50008,
         chunk_max_events=None, chunk_max_volume=None, chunk_max_time=None,
         overwrite=True,
         split=False,
         duration=None,
+        timeout=5,
         ) -> None:
         self.target_file = target_file
         self.do_overwrite = overwrite
@@ -26,8 +24,6 @@ class Receiver():
         self.port = port
         self._duration = duration
 
-
-        # self.chunk_size_bytes = chunk_size
         self.chunk_max_events = chunk_max_events # Event number
         self.chunk_max_volume = chunk_max_volume # Bytes
         self.chunk_max_time = chunk_max_time # seconds
@@ -52,12 +48,14 @@ class Receiver():
 
         self.results = {}
 
+        socket.setdefaulttimeout(timeout)
+
         if all([max is None for max in [self.chunk_max_events, self.chunk_max_volume, self.chunk_max_time]]):
-            # raise ValueError(f"Need at least one chunk-size limit.")
             print("Warning: No Chunking set.")
 
         if self.__do_split is True:
             self.target_file = f"{self.target_file}.wfm.{self.current_split:0{self.split_suffix_length}}"
+
 
     def __del__(self):
         if self.__do_readout is True:
@@ -66,7 +64,7 @@ class Receiver():
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type,exc_value, exc_traceback):
+    def __exit__(self, exc_type, exc_value, exc_traceback):
         if self.__do_readout is True:
             self.stop()
 
@@ -75,87 +73,86 @@ class Receiver():
         # capture what is normally pickled
         state = self.__dict__.copy()
 
-        # remove unpicklable/problematic variables
+        # remove unpicklable/problematic variables (thread objects)
         state['_Receiver__p_writers'] = []
         state['_Receiver__p_readout'] = None
         state['_Receiver__p_update'] = None
 
         return state
 
-    def _signal_handler(self, signal, frame):
-        print("")
-        print("Catched Keyboard Interrupt")
-        self.stop()
 
+    def __signal_handler(self, signal, frame):
+        print("")
+        print("Receiver caught Keyboard Interrupt")
+        self.stop()
 
     def start(self, duration=None):
         self.__start(duration=duration)
 
-        # signal.signal(signal.SIGINT, self._signal_handler)
-        # forever = mp.Event()
-        # forever.wait()
+        signal.signal(signal.SIGINT, self.__signal_handler)
 
     def __start(self, duration=None):
-        # mp.set_start_method('spawn')
-
-        self.__start_socket()
         self.__do_readout = True
+        self.__start_socket()
 
-        self.__data_queue = mp.Queue() # maxsize is 2147483647
-        self.__update_queue = mp.Queue() # maxsize is 2147483647
+        self.__data_queue = queue.Queue() # maxsize is 2147483647
+        self.__update_queue = queue.Queue() # maxsize is 2147483647
 
-        self.__p_update = mp.Process(name="p_update", target=self._update_received_data)#, args=(pipe_rec))
+        self.__p_update = thr.Thread(name="p_update", target=self._update_received_data)#, args=(pipe_rec))
         self.__p_update.start()
 
-        self.__p_readout = mp.Process(name="p_readout", target=self._readout)
+        self.__p_readout = thr.Thread(name="p_readout", target=self._readout)
         self.__p_readout.start()
 
         self.__new_writer_process()
 
-
-        duration = duration if duration is not None else self._duration if self._duration is not None else None
+        duration = (duration if duration is not None else
+                    self._duration if self._duration is not None else
+                    None)
         if duration is not None:
             time.sleep(duration)
-            print("Reached end of timer")
-            print("Reached end of timer")
             print("Reached end of timer")
             self.stop()
 
 
     def stop(self):
-        # print(mp.active_children())
+        #
+        # Alternative: Use this function to send a last package to the
+        #   readout and queues in order to avoid try/excepts and timeouts...
+        # socket.socket(socket.AF_INET, socket.SOCK_DGRAM).sendto("".encode(), ("localhost", self.port))
+        #
+
+        print(thr.enumerate())
         self.__do_readout = False
 
-        while self.__data_queue.empty() is False or self.__update_queue.empty() is False:
-            # print("Waiting for queues to be emptied...")
-            time.sleep(1)
+        if self.__data_queue is not None:
+            print("Closing data queue")
+            self.__data_queue.join()
+        if self.__update_queue is not None:
+            print("Closing update queue")
+            self.__update_queue.join()
+
+        for t in thr.enumerate():
+            if t.name in ["MainThread", "QueueFeederThread"]:
+                continue
+            # print(f"Terminating {t}")
+            t.join()
 
         if self.__sock is not None:
             # print("Shutting down socket")
             self.__sock.shutdown(socket.SHUT_RD)
             self.__sock.close()
 
-        for p in mp.active_children():
-            # print(f"Terminating {p}")
-            p.terminate()
-            p.join()
-
-        if self.__update_queue is not None:
-            # print("Closing update queue")
-            self.__update_queue.close()
-        if self.__data_queue is not None:
-            # print("Closing data queue")
-            self.__data_queue.close()
-
-        if len(mp.active_children()) > 0:
-            print(mp.active_children())
+        thr_list =[t for t in thr.enumerate() if t != thr.main_thread()]
+        if len(thr_list) > 0:
+            print(thr.enumerate())
         else:
-            print("Receiver: Subprocesses, queues and sockets closed succesfullly.")
+            print("Receiver: Subprocesses, queues and sockets all closed succesfullly.")
 
         return self.results
 
 
-    def check_chunk_condition(self, count=0, total_data=0, run_time=0):
+    def _check_chunk_condition(self, count=0, total_data=0, run_time=0):
 
         if self.__do_split is True:
             self.__change_target_file(mode="split")
@@ -201,16 +198,16 @@ class Receiver():
                         except:
                             continue
             if max(suffixes) > 0:
-                new_target = f"old_target.{max(suffixes)}"
+                new_target = f"{old_target}.{max(suffixes)+1}"
             else:
-                new_target = "old_target.1"
+                new_target = f"{old_target}.1"
 
         elif new_target is not None:
             pass
         else:
             raise ValueError(f"Enter new target for {self.target_file}.")
 
-        old_writer.stop()
+        old_writer.join()
         self.__new_writer_process(filename=new_target)
 
         return new_target
@@ -220,20 +217,20 @@ class Receiver():
         if target is None:
             target = self._write_to_file
 
-        print("new readout process", kwargs)
-        p = mp.Process(name=f"p_writer_{len(self.__p_writers    )}",target=target, args=(kwargs))
+        print("new writer process", kwargs)
+        t = thr.Thread(name=f"p_writer_{len(self.__p_writers    )}",target=target, args=(kwargs))
         self.__p_writers.append(p)
-        p.start()
-        print(mp.active_children())
-        return p
+        t.start()
+        # print(thr.enumerate())
+        return t
 
     def __start_socket(self):
         self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__sock.bind((self.host, self.port))
-        # self.__sock.listen()
 
-        # print(f"Started socket at {self.__sock.getsockname()} to {self.__sock.getpeername()}.")
-        print(f"Started UDP socket at {self.__sock.getsockname()}.")
+        self.host, self.port = self.__sock.getsockname()
+
+        print(f"Started UDP socket at {self.host}:{self.port}.")
         return self.__sock
 
     def _write_to_file(self, filename=None):
@@ -243,49 +240,69 @@ class Receiver():
         if self.do_overwrite is False and  os.exists(filename):
             filename = self.__change_target_file(mode="overwrite", new_target=filename)
 
+        timeout = socket.getdefaulttimeout()
+
         with open(filename, "wb") as file:
             while self.__do_readout is True or self.__data_queue.empty() is False:
-                file.write(self.__data_queue.get())
+                try:
+                    file.write(self.__data_queue.get(block=True, timeout=timeout))
+                    self.__data_queue.task_done()
+                except queue.Empty:
+                    pass
+        print(f"Writer has closed {filename}")
 
-    def __read_to_stdout(self):
-        self.__readout(sys.stdout)
+    # def _write_to_stdout(self):
+        # self.__readout(sys.stdout)
+        # pass
 
     def _readout(self):
         while self.__do_readout is True:
-            data = self.__sock.recv(self.__split_size)
-            self.__data_queue.put_nowait(data)
-            self.__update_queue.put_nowait(len(data))
-        # else:
-        #     self.stop()
+            try:
+                data = self.__sock.recv(self.__split_size)
 
+                if len(data) > 0:
+                    self.__data_queue.put_nowait(data)
+                    self.__update_queue.put_nowait(len(data))
+            except TimeoutError:
+                pass
 
-    def _update_received_data(self):
-        count = 0
-        total_data = 0
-        start_time = time.time()
+        else:
+            print("Stopped readout")
 
-        print(f"Waiting for packages.")
+    def _update_received_data(self,
+        count = 0,
+        total_data = 0,
+        start_time = time.time(),
+        ):
+
+        run_time = 0
+        total_rate = 0
+        timeout = socket.getdefaulttimeout()
+        print(f"Waiting for packages...")
 
         while self.__do_readout is True or self.__update_queue.empty() is False:
+            try:
+                recv_data_volume = self.__update_queue.get(block=True, timeout=timeout)
 
-            # recv_data_volume = pipe_rec.recv() # Blocks until reception
-            recv_data_volume = self.__update_queue.get()
+                count += 1
+                total_data += recv_data_volume
+                run_time = time.time() - start_time
 
-            count += 1
-            total_data += recv_data_volume
-            run_time = time.time() - start_time
+                self._check_chunk_condition(count, total_data, run_time)
 
-            self.check_chunk_condition(count, total_data, run_time)
+                total_rate = total_data / run_time
 
-            total_rate = total_data / run_time
-
-            print(f"Received: {count} packages in {int(run_time)}s for {total_data} Bytes in total. ({total_rate:.2}B/s) Chunks: {self.current_chunk}, Splits:{self.current_split}",
-                end="\r",
-                # file=sys.stdout, # Necessary?
-                flush = True
-            )
+                print(f"Received: {count} packages in {int(run_time)}s for {total_data} Bytes in total. ({total_rate:.2}B/s) Chunks: {self.current_chunk}, Splits:{self.current_split}",
+                    end="\r",
+                    # file=sys.stdout, # Necessary?
+                    flush = True
+                )
+                self.__update_queue.task_done()
+            except queue.Empty:
+                pass
         else:
             if self.__update_queue.empty() is True:
+                print("Summary:")
                 print(f"Received: {count} packages in {int(run_time)}s for {total_data} Bytes in total. ({total_rate:.2}B/s) Chunks: {self.current_chunk}, Splits:{self.current_split-1}")
                 self.results = {
                     "received_packages": count,
@@ -298,9 +315,8 @@ class Receiver():
                     "host": self.host,
                     "port": self.port,
                 }
-                self.__p_update.stop()
             else:
-                self.update_received_data()
+                self.update_received_data(count=count, total_data=total_data, start_time=start_time)
 
 
 
