08feedf640dd42333a2f610be5c7f13f90cf5df4
Ulrich Pirling <ulrich.pirling@student.kit.edu>
2023-06-22 19:32:24 +0200

    improve import_helper

--- notebooks/import_helper.py
+++ notebooks/import_helper.py
@@ -4,15 +4,27 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path_to_data_parser = ".."
+def pjoin(path):
+    return os.path.join(
+        os.path.dirname(__file__),
+        path
+    )
+
+path_to_data_parser = pjoin("..")
 sys.path.append(path_to_data_parser)
 from data_parser.configuration import CONFIG
 from data_parser import struct_conversion, data_io, plotting
 from measurement_class import Measurement
 
-path_to_meas_interfaces = "../../nd-measurement-interfaces/"
+path_to_meas_interfaces = pjoin("../../nd-measurement-interfaces/")
 sys.path.append(path_to_meas_interfaces)
 from KeysightAgilent_3220A.pulse_emulation import pGen_pulse_class
+
+from KeysightAgilent_3220A import Agilent3220A
+from Tektronix_DPO4104B import TektronixDPO4104B
+
+import pyvisa
+from pyvisa.errors import VisaIOError
 
 # import(
 #     get_total_pulse_width,
@@ -23,7 +35,7 @@ from KeysightAgilent_3220A.pulse_emulation import pGen_pulse_class
 #     calculate_gap
 # )
 
-meas_base_path = "C:/Users/utrfh/WS22-23 (MA) Masterarbeit/Measurements"
+meas_base_path = pjoin(Measurement.base_path)
 irrelevant_labels = ["Type", "Subsecs", "Channel_number", "Rest", "min", "max", "trigger_count", "Multiplicity", "Trigger_info"]
 
 
@@ -41,3 +53,12 @@ def list_imports():
     for i in imports():
         print(*i)
         # print(list(imports()))
+
+def hold(message=""):
+    if message != "" and not message.endswith(" "):
+        message += " "
+    uinput = input(f"{message}Press [ENTER] to continue.")
+    if uinput in ["\n", ""]:
+        return uinput
+    else:
+        hold(message)
