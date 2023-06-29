22a93c029c9399591bff2acd8dfda8c0ab27b182
Ulrich Pirling <ulrich.pirling@student.kit.edu>
2023-06-29 09:44:54 +0200

    receiver: append file ext. if not given in switch

--- udp_receiver/receiver_class.py
+++ udp_receiver/receiver_class.py
@@ -246,6 +246,7 @@ class Receiver():
             print("Skipped catch command: socket is currently closed. Please start receiver first.")
 
     def switch_file(self, filename):
+        filename += "" if filename.endswith(".bin") else "_readout.bin"
         if not os.path.isabs(filename):
             filename = os.path.join(self.target_dir, filename)
         self.__switch_target_file(new_target=filename)
