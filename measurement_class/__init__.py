f8160f93d0876d06a1226185738f28e6bae794a8
Ulrich Pirling <ulrich.pirling@student.kit.edu>
2023-08-18 16:50:11 +0200

    migrate measurement_class to pathlib

--- measurement_class/__init__.py
+++ measurement_class/__init__.py
@@ -1,10 +1,12 @@
 """
 Class representing a single - or a suite of - measurements.
 """
-import os
+# import os
 import time
 import pickle
 import json
+
+from pathlib import Path
 
 #
 # Todo: replace point_number with suffix of variable length
@@ -113,7 +115,7 @@ class Measurement():
         self._update_property("suffixes", suffixes)
 
     def _init_with_file(self, file):
-        if not os.path.exists(file):
+        if not Path(file).exists():
             self._init_with_id(file)
             return
 
@@ -157,11 +159,13 @@ class Measurement():
 
     @property
     def path(self, key=None):
-        group_path = os.path.join(
-            self.base_path, f"{self.group} - {self.group_desc}"
+        group_path = Path(
+            self.base_path,
+            f"{self.group} - {self.group_desc}"
         )
-        subgroup_path = os.path.join(
-           group_path, f"{self.date}{self.subgroup} {self.subgroup_desc}"
+        subgroup_path = Path(
+           group_path,
+           f"{self.date}{self.subgroup} {self.subgroup_desc}"
         )
         return group_path, subgroup_path
 
@@ -258,30 +262,45 @@ class Measurement():
     def get_path(self, key, subkey):
 
         if (key, subkey) == ("commander", "conf"):
-            if not os.path.exists(self.radc_commander_config_file):
+            if not Path(self.radc_commander_config_file).exists():
                 self.make_radc_commander_config()
+        elif (key, subkey) == ("commander", "pbk"):
+            if not Path(self.subgroup_path, "radc_playbook.txt").exists():
+                return None
 
         path_keys = {
             "tek": {
-                "set": f"\"E:{self.groupid}/{self.id}_tek.set\"",
-                "img": f"\"E:{self.groupid}/{self.id}_tek.png\"",
-                "wfm": f"\"E:{self.groupid}/{self.id}_tek.isf\"",
+                "set": f"\"E:{self.groupid}/data/{self.id}_tek.set\"",
+                "img": f"\"E:{self.groupid}/data/{self.id}_tek.png\"",
+                "wfm": f"\"E:{self.groupid}/data/{self.id}_tek.isf\"",
             },
             "pgen": {
-                "file": os.path.join(self.subgroup_path, f"{self.id}_pgen.json")
+                "file": Path(self.subgroup_path, "data", f"{self.id}_pgen.json")
             },
             "commander": {
-                "pbk": os.path.join(self.subgroup_path, "radc_playbook.txt"),
-                "conf": os.path.join(self.subgroup_path, "radc_config.yaml"),
+                "pbk": Path(self.subgroup_path, "radc_playbook.txt"),
+                "conf": Path(self.subgroup_path, self.radc_commander_config_file),
+                "filter_dump": Path(
+                    self.subgroup_path,
+                    "data",
+                    f"{self.id}_radc_FilterSettings.json"
+                    #
+                    # Todo: make absolute
+                    #
+                    ),
             },
             "receiver": {
-                "root": self.group_path,
-                "dir": f"{self.date}{self.subgroup} {self.subgroup_desc}",
+                "root": self.subgroup_path,
+                # "dir": f"{self.date}{self.subgroup} {self.subgroup_desc}",
+                "dir": "data",
                 "file": f"{self.id}_readout.bin",
                 "test": f"{self.id}_readout_test.bin",
             },
             "df": {
-                "save": f"{self.subgroup_path}/{self.groupid}_DataFrame.{pickle.HIGHEST_PROTOCOL}pickle"
+                "save": Path(
+                    f"{self.subgroup_path}",
+                    f"{self.groupid}_DataFrame.{pickle.HIGHEST_PROTOCOL}pickle"
+                    )
             }
         }
 
@@ -290,15 +309,19 @@ class Measurement():
     def get_command(self, key):
         command_keys = {
             "commander": (
-                f"python -m radc_commander"
-                +f" -p \"{self.get_path('commander', 'pbk')}\""
+                "python -m radc_commander"
+                +(f" -p \"{self.get_path('commander', 'pbk')}\""
+                    if self.get_path('commander', 'pbk') else "")
                 +f" -c \"{self.get_path('commander', 'conf')}\""
                 ),
             "receiver": (
-                f"python udp_receiver"
-                +f" target_root=\"{self.group_path}\""
+                "python udp_receiver"
+                +f" target_root=\"{self.subgroup_path}\""
                 +f" target_dir=\"{self.get_path('receiver', 'dir')}\""
                 +f" target_file=\"{self.get_path('receiver', 'file')}\""
+            ),
+            "dump_filter": (
+                f"RADC save_filter_settings {self.get_path('commander', 'filter_dump')} "
             )
         }
         return command_keys[key]
@@ -309,11 +332,13 @@ class Measurement():
                 "output_settings":{
                     "temp_root": "$temp",
                     "temp_dir": "radc_commander",
-                    "data_root": self.subgroup_path,
-                    "log_root": self.subgroup_path,
+                    "data_root": str(self.subgroup_path),
+                    "log_root": str(self.subgroup_path),
                     "data_dir": "data",
                     "log_dir": "",
                     "file_name_structure": f"{self.groupid}_%basename",
+                    # "data_file_basename": f"{self.groupid}_readout.bin",
+                    # "register_db_basename": f"{self.groupid}_register_db.json",
                     "data_file_basename": "readout.bin",
                     "register_db_basename": "register_db.json",
                 }
@@ -322,6 +347,6 @@ class Measurement():
 
         filename = self.radc_commander_config_file
         with open(filename, 'w', encoding="utf-8") as file:
-            json.dump(d, file)
+            json.dump(d, file, indent=4)
         print(f"Created {filename}")
         print(json.dumps(d, indent=4))
