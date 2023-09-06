330cfed9d7d1fcd0d0954d3e5ee38a5ec8e41594
Ulrich Pirling <ulrich.pirling@student.kit.edu>
2023-09-06 13:43:05 +0200

    struct_conversions for v1 and draft v2

---
+++ data_parser/v1/struct_conversion.py
@@ -0,0 +1,18 @@
+# import struct
+# import collections
+
+# from ..configuration import CONFIG
+from ..common.struct_conversion import (
+    # endianness_struct_mapping,
+    _DataFile,
+    _Snippet
+)
+
+
+
+
+class DataFile(_DataFile):
+    pass
+
+class Snippet(_Snippet):
+    pass
