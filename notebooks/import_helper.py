5dd3db43598e8f6e1187f1f232f9814c87c72ba8
utrfh <ulrich.pirling@student.kit.edu>
2023-09-13 15:15:39 +0200

    Update import_helper.py remove merge conflict annotations

--- notebooks/import_helper.py
+++ notebooks/import_helper.py
@@ -11,11 +11,6 @@ def pjoin(path):
         path
     )
 
-<<<<<<< 34b68106b5f42cc0f95c308fa2b87f9123c5baf1
-path_to_data_parser = pjoin("..")
-sys.path.append(path_to_data_parser)
-=======
->>>>>>> 1fd1808ed53c1d0c0525d6a706be754fa4734642
 import data_parser
 data_parser.init("v1")
 from data_parser import data_io, dataFrame_helpers, plotting, struct_conversion
