ebc6acb52d3f845fb1a9e270ac56078dc07a47d6
Ulrich Pirling <ulrich.pirling@student.kit.edu>
2023-09-30 17:20:26 +0200

    migrate import_helper to __init__ file as package

--- notebooks/__init__.py
+++ notebooks/__init__.py
@@ -1,5 +1,65 @@
-from import_helper import *
+# ----------------------------------------------------------------------
+# General imports
 
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import scipy
+# import scipy.signal as scp
+
+
+# ----------------------------------------------------------------------
+# ND imports
+
+# import data_parser
+# data_parser.init("v1")
+# from data_parser import data_io, dataFrame_helpers, plotting, struct_conversion
+# from data_parser.configuration import CONFIG
+
+
+# from KeysightAgilent_3220A.pulse_emulation import pGen_pulse_class
+
+# from KeysightAgilent_3220A import Agilent3220A, pulse_emulation
+# from Tektronix_DPO4104B import TektronixDPO4104B
+
+# import baseline_analysis
+
+# import pulse_analysis
+
+# import pyvisa
+# from pyvisa.errors import VisaIOError
+
+# # import(
+# #     get_total_pulse_width,
+# #     get_pulse_integral,
+# #     get_pulse_function,
+# #     get_pulse_integral_disc,
+# #     get_total_pulse_integral,
+# #     calculate_gap
+# # )
+
+
+
+# ----------------------------------------------------------------------
+# General
+import os
+def pjoin(path):
+    return os.path.join(
+        os.path.dirname(__file__),
+        path
+    )
+
+
+# ----------------------------------------------------------------------
+# Variables
+from measurement_class import Measurement
+
+meas_base_path = pjoin(Measurement._base_path)
+irrelevant_labels = ["Type", "Subsecs", "Channel_number", "Rest", "min", "max", "trigger_count", "Multiplicity", "Trigger_info"]
+
+
+# ----------------------------------------------------------------------
+# Functions
+import types
 
 def hold(message=""):
     if message != "" and not message.endswith(" "):
@@ -9,3 +69,40 @@ def hold(message=""):
         return uinput
     else:
         hold(message)
+
+def _imports():
+    for name, val in sorted(globals().items()):
+        if name.startswith("_"):
+            continue
+        if isinstance(val, types.ModuleType):
+            yield name, val.__name__
+        elif isinstance(val, str) or isinstance(val, list):
+            yield name, val
+
+def list_imports():
+    for i in _imports():
+        print(i[0], f"\t{i[1:]}")
+        # print(list(imports()))
+
+
+# ----------------------------------------------------------------------
+# Helpers
+import json
+import numpy as np
+
+class CustomEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, o):
+        if isinstance(o, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+            return int(o)
+        elif isinstance(o, (np.float_, np.float16, np.float32,
+                              np.float64)):
+            return float(o)
+        elif isinstance(o, (np.ndarray,)):
+            return o.tolist()
+        return json.JSONEncoder.default(self, o)
+
+
+
