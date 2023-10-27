ed583c19a951aabdab1a02a8e5393cb8fc40f689
Francesco <francesco@scc-wlan-kit-cn-172-17-227-20.scc.kit.edu>
2023-10-27 11:45:17 +0200

    Removed default arguments and replaced with sys.argv

--- preprocess_data/preprocessDataset.py
+++ preprocess_data/preprocessDataset.py
@@ -2,12 +2,11 @@ from pathlib import Path
 import peak_finding_algorithms as pf
 import sys
 
-data_dir='.'
-out_dir='.'
+data_dir = sys.argv[1]
+namefile_data = sys.argv[2]
+out_dir = sys.argv[3]
+namefile_output = sys.argv[4]
+tracelength = int(sys.argv[5])
 
-namefile_data=sys.argv[1]
-namefile_output=sys.argv[2]
-tracelength=int(sys.argv[3])
-
-pf.single_dataset_to_root(data_dir,namefile_data,out_dir,namefile_output,tracelength=tracelength)
-
+pf.single_dataset_to_root(data_dir, namefile_data,
+                          out_dir, namefile_output, tracelength=tracelength)
