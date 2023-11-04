89c29b6dfc55adcb95e9cc4d21c0b3b96a966a40
Ulrich Pirling <ulrich.pirling@student.kit.edu>
2023-11-04 17:39:58 +0100

    pulse_analysis: implement saturation detection and better sign overflow correction

--- pulse_analysis/pulse_calculations.py
+++ pulse_analysis/pulse_calculations.py
@@ -2,6 +2,7 @@
 Module providing calculation functions for found pulses.
 """
 import numpy as np
+import pandas as pd
 
 
 def get_baseline_avg(row):
@@ -33,12 +34,54 @@ def add_samples(row):
 
 def fix_overflows(row, column="samples", threshold=-2000):
     """
-    Returns samples but corrected in a way that:
-    if samples_i < threshold, then return 2**13+2**13+samples_i
-    else return samples_i
+    Fix sign overflows in ADC counts (represented as 14 bit signed int).
+
+    14 bit unsigned integers have a range from 0 to 2**14 - 1.
+    14 bit signed integers have a range from -2**13 to 2**13 - 1.
+
+    This function returns the samples list corrected in a way that:
+
+    if one negative sample is more than half the value range away from
+    the previous sample, and that sample is positive;
+    then the negative sample and all following samples up to the last
+    sample before a comparable inverse jump,
+    are corrected by an offset equal to the length of the value range.
+
+    This means that sign overflows where the real value of the overflow-
+    sample is higher than half the value range plus the value of the
+    previous sample, are not detected.
     """
-    a = row[column] if column else row
-    return [i if i > threshold else 8192+8192+i for i in a]
+    samples = row[column] if column else row
+    upper_range = pd.Interval(left=2**12 -1, right=2**13 -1, closed="both") # [4095,8191]
+    lower_range = pd.Interval(left=-2**13, right=-2**12, closed="both")     # [-8192, -4096]
+    offset = (2**13) + (2**13 -1)
+
+    # early exit if no correction necessary
+    if all(s not in lower_range for s in samples):
+        return samples
+
+    # Get regions where samples are in the upper range
+    high = [i for i,s in enumerate(samples) if s in upper_range]
+    between = [
+        (high[_j-1]+1, i-1)
+        for _j,i in enumerate(high)
+        if _j>0 and i!=high[_j-1]+1
+        ]
+
+    # look at interruptions between those regions
+    need_correction =[]
+    for left_end, right_end in between:
+        if (samples[left_end] in lower_range
+        and samples[right_end] in lower_range):
+            need_correction += range(left_end, right_end+1)
+
+    # correct samples within those interruptions
+    for index in need_correction:
+        samples[index] = samples[index] + offset
+
+    return samples
+    # return [i if i > threshold else 8192+8192+i for i in a] # i is negative in else case
+
 
 
 def adjust_baseline(data):
@@ -49,3 +92,31 @@ def adjust_baseline(data):
     data["Limits"] = data["PeakFinding"].apply(get_limits)
     data["BaselineCorr"] = data.apply(get_baseline_avg, axis=1)
     data["Sum"] = data[["samples", "Limits", "BaselineCorr"]].apply(add_samples, axis=1)
+
+
+def detect_saturation(row, column="samples"):
+    """
+    Returns the list of sample indices from the samples list;
+    where each entry marks one sample index of a chain where
+        - the sample value is >= the maximum of the value range
+        AND
+        - the sample value is equal to the previous sample value or the
+            following sample value.
+
+    If no saturation is detected, the returned list is empty ([]).
+    Using pandas, test the presence/absence of saturation using
+
+        .astype(bool)
+
+    The value range is determined as 2**13-1 (for 14bit signed integers)
+    """
+    samples = row[column] if column else row
+    max = 2**13-1
+
+    return [
+        i
+        for i,s in enumerate(samples)
+        if i>0 and s>=max and (s==samples[i-1] or s==samples[i+1])
+        ]
+    # [i for j,i in enumerate(high) if j>0 and i == high[j-1]+1]
+
