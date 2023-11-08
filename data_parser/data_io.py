bd33bd1070d44411c11ac2282f1c00877614c33b
zm6876 <zm6876@kalinka5.iap.kit.edu>
2023-11-08 14:29:05 +0100

    Corrected error in make total rootfile

--- data_parser/data_io.py
+++ data_parser/data_io.py
@@ -45,5 +45,6 @@ def make_total_dataFrame(files: list|str) -> pd.DataFrame:
 def make_total_rootfile(files:list|str,out_dir: str = '.',namefile_output: str = 'test'):
 
     df=make_total_dataFrame(files)
+    df=pf.update_dataframe_with_pulses(df)
     pf.df_to_root_file(df,out_dir,namefile_output)
 
