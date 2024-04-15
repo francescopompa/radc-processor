13bd5e174ec6e1eedf780a722925d04ca3afedf2
zm6876 <zm6876@kalinka5.iap.kit.edu>
2024-04-15 11:24:25 +0200

    Changed value of height parameter and added PostTriggerTime

--- preprocess_data/Parameters.py
+++ preprocess_data/Parameters.py
@@ -3,7 +3,7 @@ sp_height = 10
 sp_width = int(12)
 sp_distance = int(2)  # samples
 
-height = 50  # it was 0.6 mV for now it's in ADC counts
+height = 20  # it was 0.6 mV for now it's in ADC counts
 width = int(12)
 
 number_of_sample_below_thres_for_range = int(3)
@@ -12,3 +12,4 @@ sample_width = int(16)  # ns
 n_samples_baseline = int(5)
 gain = 'matched'
 T_time = 10
+PostTriggerTime = 12500
