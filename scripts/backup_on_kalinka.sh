de986dc7ae2fd1ea1f70e7b565246392fab15e03
Francesco Pompa <francesco.pompa@kit.edu>
2024-10-22 16:31:56 +0200

    Corrected the script for backup. Need to find a way to use command line arguments

--- scripts/backup_on_kalinka.sh
+++ scripts/backup_on_kalinka.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-rsync -cavu --info=progress2 /data/DAQMeasurements/ zm6876@kalinka5.iap.kit.edu:/kalinka/storage/darkmatter/lngs-neutron-detector/DAQMeasurements 
+directory="DAQMeasurements"
+
+rsync -cavu --info=progress2 /data/$directory zm6876@kalinka5.iap.kit.edu:/kalinka/storage/darkmatter/lngs-neutron-detector/ 
 
 uuid=331C2D4743721BBE
 if lsblk -f | grep -wq $uuid; then
 	echo "SSD connected"
-	rsync -cavu --info=progress2 /data/DAQMeasurements/ /media/mnd/backup/DAQMeasurements
+	rsync -cavu --info=progress2 /data/$directory /media/mnd/backup/
 else
 	echo "SSD is not connected. Please connect to backup the data"
 fi
