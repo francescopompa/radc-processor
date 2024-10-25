43120b0839f5398982ec2b504c99ff489392804d
zm6876 <zm6876@kalinka5.iap.kit.edu>
2024-10-25 10:08:25 +0200

    Removed useless sudo to install the scripts

--- install_scripts.sh
+++ install_scripts.sh
@@ -5,6 +5,6 @@ chmod +x ./scripts/backupData.sh
 chmod +x ./scripts/preprocessData.sh
 chmod +x ./scripts/dataTaking.sh
 
-sudo ln -rsf ./scripts/backupData.sh ~/.local/bin/backupData.sh
-sudo ln -rsf ./scripts/preprocessData.sh ~/.local/bin/preprocessData.sh
-sudo ln -rsf ./scripts/dataTaking.sh ~/.local/bin/dataTaking.sh
+ln -rsf ./scripts/backupData.sh ~/.local/bin/backupData.sh
+ln -rsf ./scripts/preprocessData.sh ~/.local/bin/preprocessData.sh
+ln -rsf ./scripts/dataTaking.sh ~/.local/bin/dataTaking.sh
