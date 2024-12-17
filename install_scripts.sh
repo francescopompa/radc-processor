45eb5803b514633991ed571625302779c8d61809
zm6876 <zm6876@kalinka5.iap.kit.edu>
2024-12-17 13:30:32 +0100

    Added new script to preprocess data via singularity

--- install_scripts.sh
+++ install_scripts.sh
@@ -4,7 +4,9 @@
 chmod +x ./scripts/backupData.sh
 chmod +x ./scripts/preprocessData.sh
 chmod +x ./scripts/dataTaking.sh
+chmod +x ./scripts/preprocessDataSingularity.sh
 
 ln -rsf ./scripts/backupData.sh ~/.local/bin/backupData.sh
 ln -rsf ./scripts/preprocessData.sh ~/.local/bin/preprocessData.sh
 ln -rsf ./scripts/dataTaking.sh ~/.local/bin/dataTaking.sh
+ln -rsf ./scripts/preprocessDataSingularity.sh ~/.local/bin/preprocessDataSingularity.sh
---
+++ scripts/preprocessDataSingularity.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --job-name=Preprocessing
+#SBATCH --partition=tesla.long
+#SBATCH --cpus-per-task=16
+#SBATCH --output=/kalinka/storage/darkmatter/lngs-neutron-detector/%u/jobs/results_%j.out
+#SBATCH --time=2-00:00:00 ##Runtime in D-HH:MM
+
+# The image should first be installed via the script in the ALMOND singularity repository
+
+if [ -z "$1" ]; then
+  set -- "${1:-FNG}"
+fi
+
+singularity exec --bind /kalinka/:/kalinka/ preprocessData.simg preprocessData ${1}
