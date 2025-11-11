93d09d01ea9154b23674c6ad428668efa3ee4290
Francesco Pompa <francesco.pompa@kit.edu>
2025-11-11 16:42:17 +0100

    Removed checksum option to rsync to speed up the backup

--- scripts/backupData.sh
+++ scripts/backupData.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 if [ -z "$1" ]; then
-  set -- "${1:-DAQMeasurements}"
+  set -- "${1:-ALMOND}"
 fi
 
 echo "Copying folder /data/${1} on Kalinka"
 
-rsync -cavu --info=progress2 /data/${1}/ zm6876@kalinka5.iap.kit.edu:/kalinka/storage/darkmatter/lngs-neutron-detector/${1}
+rsync -avu --info=progress2 /data/${1}/ zm6876@kalinka5.iap.kit.edu:/kalinka/storage/darkmatter/lngs-neutron-detector/${1}
 
 uuid=F43E88583E8815B0
 if lsblk -f | grep -wq $uuid; then
 	echo "External hard disk connected: backing up..."
-	rsync -cavu --info=progress2 /data/${1}/ /media/mnd/backup/${1}
+	rsync -avu /data/${1}/ /media/mnd/backup/${1}
 else
 	echo "The external HDD is not connected. Please connect to backup the data"
 fi
