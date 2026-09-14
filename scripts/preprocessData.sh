87af1fcbc4c38e1e4c289d9f32ede625969f3f27
Francesco Pompa <pompafra@linux108.lngs.infn.it>
2026-09-14 10:07:59 +0200

    Updated the processing scripts to adapt them to the U-LITE environment

--- scripts/preprocessData.sh
+++ scripts/preprocessData.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-#SBATCH --job-name=preprocessing
-#SBATCH --partition=tesla.long
-#SBATCH --cpus-per-task=4
-#SBATCH --time=7-00:00:00
-#SBATCH --output=/kalinka/storage/darkmatter/lngs-neutron-detector/%u/jobs/results_%j.out
-
 #PBS -N Preprocessing
-#PBS -o /users/p/pompafra/ALMOND/jobs/results.out
+#PBS -o /users/p/$USER/ALMOND/jobs/results.out
 #PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=4:mem=16gb
 
 # it must be run as the following:
 # qsub -F "-f -r ALMOND/sensitivity_GATOR" preprocessData.sh
 
-# Go to the directory where you submitted the job
-cd $PBS_O_WORKDIR
+exec 1>/nfs/scratch/$USER/jobs/results_$PBS_JOBID.out
+exec 2>/nfs/scratch/$USER/jobs/results_$PBS_JOBID.err
+
+__conda_setup="$('/nfs/almond/$USER/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/nfs/almond/$USER/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/nfs/almond/$USER/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/nfs/almond/$USER/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
 
 eval "$(conda shell.bash hook)"
 conda activate ALMOND
-
 preprocessData "$@"
--- scripts/preprocessDataSingularity.sh
+++ scripts/preprocessDataSingularity.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
-
-#SBATCH --job-name=Preprocessing
-#SBATCH --partition=tesla.long
-#SBATCH --cpus-per-task=16
-#SBATCH --output=/kalinka/storage/darkmatter/lngs-neutron-detector/%u/jobs/results_%j.out
-#SBATCH --time=24:00:00 ##Runtime in D-HH:MM:SS
+#!/bin/bash
 
 #PBS -N Preprocessing
-#PBS -o /users/p/pompafra/jobs/results.out
+#PBS -o /nfs/scratch/$USER/jobs/results_$PBS_JOBID.out
 #PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=4:mem=16gb
+
+# it must be run as the following:
+# qsub -F "-f -r ALMOND/sensitivity_GATOR" preprocessData.sh
+
+exec 1>/nfs/scratch/$USER/jobs/results_$PBS_JOBID.out
+exec 2>/nfs/scratch/$USER/jobs/results_$PBS_JOBID.err
+
 
 # The image should first be installed with the script in the ALMOND singularity repository
 
-singularity exec --bind /kalinka/:/kalinka/ /users/p/pompafra/software/ALMOND.simg preprocessData "$@"
+singularity exec --bind /nfs/:/nfs/ /nfs/almond/ALMOND_ULITE.simg preprocessData "$@" 
